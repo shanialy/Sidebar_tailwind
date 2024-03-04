26c7da5d8eff7a0a02e279eb15cc5fcd195a7332
Shani <97959691+shanialy@users.noreply.github.com>
2024-03-04 13:59:07 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,3 +9,14 @@ module.exports = {
     require('flowbite/plugin')
   ],
 }
+module.exports = {
+  content: [
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
