b94641eba6ff2c6f75209d9ae2fb8a8773f0ff22
Shani <97959691+shanialy@users.noreply.github.com>
2024-05-22 18:51:11 +0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -4,21 +4,3 @@ module.exports = {
     autoprefixer: {},
   },
 }
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
