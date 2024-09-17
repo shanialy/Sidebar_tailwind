6c4916980e6ec713b6dcbda1d4794a1dbe8960c4
Shani <97959691+shanialy@users.noreply.github.com>
2024-09-17 15:49:46 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,3 +9,54 @@ module.exports = {
     require('flowbite/plugin')a
   ],
 }
+module.exports = {
+  content: [
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')a
+  ],
+}module.exports = {
+  content: [
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')a
+  ],
+}module.exports = {
+  content: [
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')a
+  ],
+}module.exports = {
+  content: [
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')a
+  ],
+}module.exports = {
+  content: [
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')a
+  ],
+}
